aafe07076955038b583691e9d46104a64ffd0529
Saranya Manoj <saranyats.87@gmail.com>
2023-08-07 19:25:21 -0400

    Translink updated

--- src/main/java/MainPage/translink.java
+++ src/main/java/MainPage/translink.java
@@ -17,15 +17,15 @@ public class translink {
     static WebDriver driver;
 
     public static void main(String[] args) {
-
+        //Launch any browser
         driver = new ChromeDriver();
         // driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
-        //Navigate to URL
+        //goto https://www.translink.ca/
         driver.get("https://www.translink.ca/");
-        //Click on Schedule and Maps
+        //2)Expand the top menu, select “Schedules and Maps > Bus”
         WebElement ScheduleMaps = driver.findElement(By.cssSelector("a[href='https://www.translink.ca/schedules-and-maps']"));
         Actions actions = new Actions(driver);
         actions.moveToElement(ScheduleMaps).build().perform();
@@ -36,44 +36,44 @@ public class translink {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[href='https://www.translink.ca/schedules-and-maps/bus-schedules']")));
         actions1.moveToElement(bus).build().perform();
         bus.click();
-        //Verified page loaded succesfully
+        //3)Validate the page is loaded
         String result = driver.findElement(By.id("bus-schedules")).getText();
         if (result.equals("Bus Schedules")) {
             System.out.println("Page loaded successfully");
         }
-        //search 99
+        //4)Locate input area “Search by transit mode, route # or name”
+        //5)Input “99” and click “Find Schedule” button
         driver.findElement(By.id("find-schedule-searchbox")).sendKeys("99");
         //click on Find schedule
         driver.findElement(By.cssSelector("section:nth-child(4) > div:nth-child(2) > button:nth-child(2)")).click();
-        //click on #99-UBC B-Line
-       WebElement ubcbline = driver.findElement(By.cssSelector("a[href='https://www.translink.ca/schedules-and-maps/route/99/direction/1/schedule']"));
-        String route = ubcbline.getText();
-        ubcbline.click();
-        //Schdulelist 3 stops picked
+        //6)In the result list, select and click “#99 – UBC B-Line”
+        driver.findElement(By.cssSelector("a[href='https://www.translink.ca/schedules-and-maps/route/99/direction/1/schedule']")).click();
+        //7)In the bus schedule page of “99”,a.	Update the date
+        //8)In the schedule list, pick up following stops:a.50913 b.50916 c.58613
         driver.findElement(By.cssSelector("button[aria-label='No options selected']")).click();
         //WebElement stopName = driver.findElement(By.cssSelector("label > span"));
         List<WebElement> stops = driver.findElements(By.cssSelector("#StopsPicker-listbox>li"));
         stops.get(6).click();
         stops.get(7).click();
         stops.get(8).click();
-        //click on selected Stops only
+        //d.Click on “Selected Stops Only” button
         driver.findElement(By.cssSelector("section.CopyMain > div:nth-child(5) > button:nth-child(3)")).click();
-        //Add to favourites
+        //9)Click on “Add to favourites” button on the right side
         driver.findElement(By.cssSelector(".AddDelGTFSFav[data-type='unstyledButton'][data-faux-type='link']")).click();
         Set<String> allWindows = driver.getWindowHandles();
         for (String win : allWindows) {
             driver.switchTo().window(win);
         }
+        //10)Rename it to be “99 UBC B-Line – Morning Schedule” and click on “Add to favourites”
         driver.findElement(By.id("newfavourite")).clear();
-        driver.findElement(By.id("newfavourite")).sendKeys(route);
+        String scheduleName = "99 UBC B-Line – Morning Schedule";
+        driver.findElement(By.id("newfavourite")).sendKeys(scheduleName);
         driver.findElement(By.cssSelector("dialog[id='add-to-favourites_dialog'] button[value='confirm']")).click();
-        //Goto Manage fav to validate favourited added or not
+        //11)Click on “Manage my favourites” button on the right side to validate the item just added.
         driver.findElement(By.cssSelector("#information > section.CopyMain > div > a:nth-child(3)")).click();
         String Fav = driver.findElement(By.cssSelector("div.flexContainer.flexColumn.useAllInfinitely > ul > li > a")).getText();
-//        System.out.println(Fav);
-//        System.out.println(route);
-        if (Fav.equals(route)) {
-            System.out.println("Favourite succesfully added");
+        if (Fav.equals(scheduleName)) {
+            System.out.println("Favourite successfully added");
         }
         else{
             System.out.println("Favourite not added");
